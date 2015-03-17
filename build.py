ef44d4f8076bd08a3721b5f10bae4f06b169ead1
Adrian Green <harvardsommer@gmx.net>
2015-03-17 14:47:56 +0000

    Modified build.py

--- build.py
+++ build.py
@@ -1,17 +1,49 @@
-from pybuilder.core import use_plugin, init
+from pybuilder.core import Author, init, use_plugin
 
 use_plugin("python.core")
+
+use_plugin('copy_resources')
+
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 
+use_plugin('pypi:pybuilder_header_plugin')
+use_plugin('pypi:pybuilder_release_plugin')
 
 name = "biocuration"
-default_task = "publish"
+version = "0.1"
+
+authors = [Author("kp14", "harvardsommer@gmx.net")]
+license = 'Apache License, Version 2.0'
+url = "https://bitbucket.org/kp14/biocuration"
+description = __doc__
+summary = "Tools for biocuration."
+
+default_task = ["publish"]
 
 
 @init
 def set_properties(project):
-    pass
+    project.build_depends_on('coverage')
+    project.set_property('coverage_break_build', False)
+
+    project.build_depends_on('wheel')
+
+    project.set_property('copy_resources_target', '$dir_dist')
+    project.get_property('copy_resources_glob').append('LICENSE.txt')
+    project.get_property('copy_resources_glob').append('setup.cfg')
+
+    project.set_property('flake8_verbose_output', True)
+    project.set_property('flake8_break_build', False)
+    project.set_property('flake8_include_test_sources', True)
+
+    project.set_property('pybuilder_header_plugin_break_build', False)
+    project.set_property('pybuilder_header_plugin_expected_header', open('header.py').read())
+
+    project.set_property('distutils_commands', 'bdist_wheel')
+    project.set_property('distutils_classifiers', [
+        "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: Apache Software License'])
