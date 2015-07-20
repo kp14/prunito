f496797400913f077e79a854ed73b24597f40c70
Adrian Green <harvardsommer@gmx.net>
2015-07-20 13:20:08 +0100

    Removed further unneccessary code from module test_uniprotkb_multi.py

--- tests/test_uniprotkb_multi.py
+++ tests/test_uniprotkb_multi.py
@@ -8,15 +8,6 @@ Created on Mon May 18 11:59:36 2015
 
 @author: kpichler
 """
-from unittest import TestCase
-
-#from Bio import SwissProt
-#import biocuration.uniprotkb.parsing as up
-
-# Some diffs are very long here if tests fail so we set this
-# to None to display the whole diff
-TestCase.maxDiff = None
-
 
 def test_compare_annotations(my_annotation, biopy_annotation):
     assert my_annotation == biopy_annotation
