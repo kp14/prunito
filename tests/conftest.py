39b29acad0c7a824b8c1576f4d0d28484f4bd1fe
Adrian Green <harvardsommer@gmx.net>
2015-07-20 13:10:04 +0100

    Implemented a special case in test generation which creates one test each for each comment contained in a given entry's comment list.

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,9 +41,24 @@ with open(datafile, "r", encoding="ascii") as data:
     biopython_records = list(SwissProt.parse(data))
 
 def pytest_generate_tests(metafunc):
+    '''Automated generation of tests based on input of multiple UniProt entries.
+
+    This targets the metafunc `test_compare_annotations` in module
+    test_uniprotkb_multi.py which just compare two objects for identity, mostly
+    strings. Each individual pience of annotation in the provided UniProt entries
+    is used to generate its own test which makes pinpointing errors easier and
+    saves having to code the test manually. We can also use an arbitray set of
+    entries to test.
+    '''
     if 'my_annotation' and 'biopy_annotation' in metafunc.fixturenames:
         argvalues = []
         for my_record, record in zip(my_records, biopython_records):
             for prop in PROPS:
-                argvalues.append((getattr(my_record, prop), getattr(record, prop)))
+                if prop == 'comments':
+                    comments = []
+                    for my_comment, biopy_comment in zip(getattr(my_record, prop), getattr(record, prop)):
+                        comments.append((my_comment, biopy_comment))
+                    argvalues.extend(comments)
+                else:
+                    argvalues.append((getattr(my_record, prop), getattr(record, prop)))
         metafunc.parametrize(['my_annotation', 'biopy_annotation'], argvalues)
