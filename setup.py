4999be354ba48c096d421e302032e81bcac52d24
Adrian Green <harvardsommer@gmx.net>
2015-07-21 16:26:30 +0100

    Adopted pip's way of including version info in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
+import codecs
+import os
+import re
+
 from setuptools import setup
 
-def readme():
-    with open('README') as f:
-        return f.read()
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    # Taken from pip setup.py
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+def find_version(*file_paths):
+    # Taken from pip setup.py
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+long_description = read('README')
 
 setup(name='biocuration',
-      version='0.6.3',
+      version=find_version('biocuration', '__init__.py'),
       description='A collection of tools for protein biocuration.',
-      long_description=readme(),
+      long_description=long_description,
       classifiers=[
         'Development Status :: 4 - Beta',
         'License :: Public Domain',
