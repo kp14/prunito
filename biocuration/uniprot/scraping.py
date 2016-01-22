20e599a6e0c105818384aad555788cb6cb57a60f
kp14 <kp14@ebi>
2016-01-22 14:16:32 +0000

    Improve siganture of method get_lineage.

--- biocuration/uniprot/scraping.py
+++ biocuration/uniprot/scraping.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Some UniProt data are not available via the REST API, these might be scraped.
+Some UniProt data are not available via the REST API, these can be scraped.
 
 Created on Fri Jan 22 13:04:00 2016
 
@@ -15,12 +15,13 @@ from ..utils import UNIPROT_TAXONOMY
 session = requests.Session()
 
 
-def get_lineage(taxid, short=False):
+def get_lineage(taxid, full=True):
     '''Retrieve taxonomic lineage of given taxid.
 
     Parameter:
         taxid: taxid string, e.g. `9606`
-        short: boolean; UniProtKB entries contain a shortened lineage
+        full: boolean; UniProtKB entries contain a shortened lineage which can
+                       be retrieved by setting full=False
     Returns:
         list of taxon nodes (names)
     '''
@@ -31,6 +32,6 @@ def get_lineage(taxid, short=False):
         # Using this attribute I figuered out by experimenting
         arefs = soup.findAll(property='rdfs:subClassOf')
         # text: contains the node name, attrs[ref] the taxid
-        if short:
-            return [(ref.text, ref.attrs['href']) for ref in arefs if not 'style' in ref.attrs]
-        return [(ref.text, ref.attrs['href']) for ref in arefs]
+        if full:
+            return [(ref.text, ref.attrs['href']) for ref in arefs]
+        return [(ref.text, ref.attrs['href']) for ref in arefs if not 'style' in ref.attrs]
