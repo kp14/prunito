adbb1dc6f3a991502b73065940023b1177c41b7e
kp14 <kp14@ebi>
2016-02-09 16:37:45 +0000

    Add a few tests for the UniRule parser.

--- tests/test_unirule_parser.py
+++ tests/test_unirule_parser.py
@@ -18,28 +18,65 @@ from biocuration.uniprot import parse_rules
 
 # Import paths for data files
 base_dir = os.path.dirname(__file__)
-filename = 'made_up.xml'
+filename = 'selected-rules.xml'
 data_file = os.path.join(base_dir, 'UniRule', filename)
 
 rule = list(parse_rules(data_file))[0]
 
-def test_meta_info():
-    assert(rule.meta['id'], 'UR000150961')
-    assert(rule.meta['creator'], 'kpichler')
-    assert(rule.meta['status'], 'TEST')
+def test_meta_id():
+    assert(rule.meta['id'], 'UR000172772')
+
+
+def test_meta_creator():
+    assert(rule.meta['creator'], 'amcdouga')
+
+def test_meta_mod_by():
+    assert(rule.meta['modifiedBy'], 'h.zellner')
+
+def test_meta_status():
+    assert(rule.meta['status'], 'APPLY')
+
+def test_meta_created():
+    assert(rule.meta['created'], '2015-08-13+01:00')
+
+def test_meta_old_id():
+    assert(rule.meta['oldRuleNum'], 'RU362117')
+
+def test_meta_uniprot_id():
+    assert(rule.meta['uniprotId'], 'Cyt_b')
+
+def test_meta_dataclass():
+    assert(rule.meta['dataClass'], 'Protein')
 
 
 def test_length_conditions_sets_main():
-    assert(len(rule.main.conditions), 3)
+    assert(len(rule.main.conditions), 1)
+
+def test_main_cond_pfam():
+    assert(rule.main.conditions[0][0].type, 'Pfam id')
+    assert(rule.main.conditions[0][1].type, 'Pfam id')
+
+def test_main_cond_iter_negative():
+    for c in rule.iter_conditions():
+        assert(c.negative, False)
+
+def test_main_cond_taxon():
+    assert(rule.main.conditions[0][2].type, 'taxon')
+    assert(rule.main.conditions[0][2].value, 'Eukaryota')
+
+def test_main_cond_mito():
+    assert(rule.main.conditions[0][3].type, 'gene_location')
+    assert(rule.main.conditions[0][3].value, 'Mitochondrion')
+    assert(rule.main.conditions[0][3].negative, False)
 
 
 def test_number_annotations_main():
-    assert(len(rule.main.annotations), 2)
+    assert(len(rule.main.annotations), 13)
 
 
 def test_type_annotations_main():
-    assert(rule.main.annotations[0]['type_'], 'subcellularLocation')
+    assert(rule.main.annotations[0].type, 'recommendedName')
 
 
 def test_class_annotations_main():
-    assert(rule.main.annotations[0]['class_'], 'comment')
+    assert(rule.main.annotations[0].class_, 'protein')
