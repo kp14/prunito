fa22988e596ca38cd525a4ad38932224d7207ce1
kp14 <kp14@ebi>
2017-03-13 16:19:09 +0000

    Try another fix of the bug in parse_txt.

--- biocuration/uniprot/parser_knowledgebase_txt.py
+++ biocuration/uniprot/parser_knowledgebase_txt.py
@@ -336,7 +336,7 @@ def _flatten_lists(lol):
 
 def parse_txt(handle):
     bag, context = _set_up()
-    if not handle:s
+    if not handle:
         print('Nothing to parse. Are you sure the query returns anything?')
     for line in handle:
         line_type, line = line[:2], line[5:]
