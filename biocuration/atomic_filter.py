3d4cb989099592d5150be7c5a6725f16c71ec551
kp14 <kp14@ebi>
2017-09-20 13:43:18 +0100

    Delete atomic_filter.py

--- biocuration/atomic_filter.py
+++
@@ -1,9 +0,0 @@
-from biocuration import uniprot as up
-from biocuration.uniprot.atomic import APile
-
-
-p = APile()
-with open('/home/klemens/Downloads/allnew.txl', 'r', encoding='ascii') as infile:
-    for entry in up.parse_txt_compatible(infile):
-        p.consume(entry)
-    print(p.size())
