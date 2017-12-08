45a46a70ec4c8324a151a18b9338511c63d3b2ce
kp14 <kp14@ebi>
2017-12-08 16:54:13 +0000

    Bump version to 0.90.0 due to name change etc.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
-long_description = read('README')
+long_description = read('README.md')
 
 setup(name='prunito',
       version=find_version('prunito', '__init__.py'),
