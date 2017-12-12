7be9d5295e12000d361839e4996b3a0a6ed769e3
kp14 <kp14@posteo.de>
2017-12-12 11:07:30 +0000

    Move UniProt API calls into rest subpackage.

--- prunito/uniprot/__init__.py
+++ prunito/uniprot/__init__.py
@@ -3,15 +3,15 @@
 
 from .parser_knowledgebase_txt import parse_txt, parse_txt_compatible, parse_txt_atomic
 from .parser_unirule import parse_rules
-from .rest_api import (current_release,
-                       search_reviewed,
-                       search_unreviewed,
-                       search_all,
-                       number_SP_hits,
-                       retrieve_batch,
-                       convert,
-                       map_id,
-                       )
+from .rest.uniprotapi import (current_release,
+                              search_reviewed,
+                              search_unreviewed,
+                              search_all,
+                              number_SP_hits,
+                              retrieve_batch,
+                              convert,
+                              map_id,
+                              )
 from .rest.proteinsapi import (
                                 get_info_on_taxID,
                                 get_info_on_taxIDs,
